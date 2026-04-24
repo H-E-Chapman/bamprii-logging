43f6a4c6ac06e67978e86791267fc467a69d82b7
HECha <Harry.Chapman.22@ucl.ac.uk>
2026-04-24 13:48:46 +0100

    added link to sheet

--- app.py
+++ app.py
@@ -105,6 +105,8 @@ with st.sidebar:
         _reset_fields(groups, logger)
         st.rerun()
 
+    st.markdown("[Logging Sheet](https://docs.google.com/spreadsheets/d/12aXiQaNwGc0lGcMVU-gu18zWZClgI-FNx0RuSCTF_28/edit?gid=1925851779#gid=1925851779)")
+
     st.markdown("---")
     st.subheader("Active Equipment")
     st.caption("Toggle which variable sets are relevant for this session.")
